60f29446309ccddf15ea55080fed316778a45fda
Fabian <fabianl.bartik@yahoo.de>
2022-04-30 19:25:09 +0200

    v0.1.4

---
+++ src/Navigation.js
@@ -0,0 +1,29 @@
+import { Navbar } from 'react-bootstrap';
+import { Nav } from 'react-bootstrap';
+import Container from 'react-bootstrap/Container';
+import App from './App';
+
+export default function Navigation(){
+    return(
+    <>
+      <Navbar bg="dark" variant="dark">
+        <Container>
+        <img
+          src="logo-lila.png"
+          width="30"
+          height="30"
+          
+          className="d-inline-block align-top"
+          alt="React Bootstrap logo"
+        />
+        <Navbar.Brand href="#home">Projekt23</Navbar.Brand>
+        <Nav className="me-auto">
+          <Nav.Link href="/">Home</Nav.Link>
+          <Nav.Link href="features">Features</Nav.Link>
+          <Nav.Link href="pricing">Pricing</Nav.Link>
+        </Nav>
+        </Container>
+      </Navbar>
+    </>
+    )
+  }
