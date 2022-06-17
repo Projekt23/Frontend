5f1bea3733f448fca483346c09056a4ffaf15304
maxiItheIhunter <Maximilian.Holzmann@gmx.de>
2022-06-17 11:05:59 +0200

    Login Responsive

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -14,7 +14,7 @@ const Login = ({setUserID, password, setPassword}) => {
     const [saveUsername, setSaveUsername] = useState(false);
     const gridStyle = {height: "100%", width: "100%"}
     const paperStyle = {padding: 20, height: '65vh', width: "25%", margin: "80px auto"}
-    const paperStyleMobile = {padding: 20, height: '65vh', width: "80%", margin: "80px auto"}
+    const paperStyleMobile = {padding: 20, height: '65vh', width: "50%", margin: "80px auto"}
     const logoStyle = {display: "flex", justifyContent: "center", alignItems: "center",}
     const textfieldStyle = {margin: "5px"}
     const divloginbtn = {width: "100%", alignItems: "center", flexDirection: "column",}
@@ -29,7 +29,7 @@ const Login = ({setUserID, password, setPassword}) => {
 
 
     function checkResponsive(){
-        if(window.innerWidth > 1000){
+        if(window.innerWidth > 1590){
         return paperStyle
         }
         else{
@@ -106,7 +106,7 @@ const Login = ({setUserID, password, setPassword}) => {
     return (
         <div style={{ width: '100vw',
         height: '100vh', backgroundImage: "url(background.png)", backgroundRepeat: 'no-repeat', backgroundPosition:"center", backgroundSize:"100%"}}>
-        <Card style={checkResponsive()} >
+        <Card style={checkResponsive()} sx={{display: {xs: 'none', md: 'flex'}}} >
             <Grid
                 container
                 direction="column"
@@ -180,6 +180,83 @@ const Login = ({setUserID, password, setPassword}) => {
                         </Typography>
                     </div>
                 </Grid>
+            </Grid>
+        </Card>
+
+        <Card style={{padding: 20, height: '65vh', width: "75%", margin: "80px auto"}} sx={{display: {xs: 'flex', md: 'none'}}} >
+            <Grid
+                container
+                direction="column"
+                justifyContent="space-between"
+                alignItems="center"
+                style={gridStyle}
+            >
+                <Grid item direction="row" alignContent="center">
+                    <div style={logoStyle}>
+                        <img src="logo-lila.png" alt="logo" width="50px" h
+                        eight="50px"/>
+                    </div>
+                </Grid>
+                <Grid item >
+                    <Typography variant="h5" secondary>Anmeldung </Typography>
+                    
+                    <TextField
+                        variant="standard"
+                        fullWidth style={textfieldStyle}
+                        label='Benutzername oder Email *'
+                        defaultValue={localStorage.getItem("userName")}
+                        placeholder='Benutzername oder Email eingeben ...'
+                        onChange={(event) => setUserName(event.target.value)}
+                        onKeyPress={(ev) => {
+                            if (ev.key === 'Enter') {
+                                postLogin(userName, password)
+                            }
+                          }}
+                    />
+                    <TextField
+                        variant="standard"
+                        fullWidth style={textfieldStyle}
+                        type="password"
+                        label='Passwort *'
+                        placeholder='Passwort eingeben ...'
+                        onChange={(event) => setPasswordF(event.target.value)}
+                        onKeyPress={(ev) => {
+                            if (ev.key === 'Enter') {
+                                postLogin(userName, password)
+                            }
+                          }}
+                    />
+                    <FormControlLabel
+                        control={<Checkbox defaultChecked={checkSaveUsername()}
+                        onChange={(event) => {setSaveUsername(event.target.checked)}} size="small"/>} label="Nutzer merken" 
+                    > </FormControlLabel><Typography  variant="caption">* Pflichtfeld </Typography><Typography color = "red" >{ERROR}</Typography>
+                </Grid>
+               
+                <Grid item style={divloginbtn}>
+                    
+                    <Button
+                        type="submit"
+                        //color="primary"
+                        variant="contained"
+                        fullWidth
+                        onClick={() => postLogin(userName, password)}
+                        style={loginbtn}
+                    >
+                        Login
+                    </Button>
+                    
+                        
+                        
+                        
+
+                </Grid>
+                <Typography variant="caption">
+                            <Link>Passwort vergessen?</Link>
+                        </Typography>
+                <Typography variant="caption">
+                            Neu hier?
+                <Link> Anmeldelink anfordern</Link>
+            </Typography>
             </Grid>
         </Card>
          </div>
